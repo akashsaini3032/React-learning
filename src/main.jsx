79fd759a5b94326563b6176358d3ec95f407c237
akashsaini3032 <akashsainira@gmail.com>
2025-05-06 23:31:01 +0530

    update content

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,3 @@
-//  day 1
-
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import Cybrom from './cybrom.jsx'
-// import Header from './Header.jsx'
-// import Data from './Data.jsx'
-// import Footer from './Footer.jsx'
-// //component function ko bolte hai first letter capital hona chahiye
-// //file ka naam kuchh bhi ho sakta hai but better approach hai same rakho taki confusion na ho
-// createRoot(document.getElementById('root')).render(
-//  <>
-//  <App />
-//  < Cybrom/>
-//  < Header />
-//  < Data />
-//  < Footer />
-//  </>
-    
-    
-    
-  
-// )
-
-
 
 
 import { createRoot } from 'react-dom/client'
